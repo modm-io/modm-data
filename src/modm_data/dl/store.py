2a6b145461aec340c18fdcd6c690b2e0da5a7aa6
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2025-01-01 19:39:45 +0100

    [dl] Fix download headers

--- src/modm_data/dl/store.py
+++ src/modm_data/dl/store.py
@@ -12,7 +12,6 @@ _hdr = {
     "Sec-Fetch-Site": "none",
     "Accept-Encoding": "identity",
     "Sec-Fetch-Mode": "navigate",
-    "Host": "www.st.com",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15",
     "Accept-Language": "en-GB,en;q=0.9",
     "Sec-Fetch-Dest": "document",
