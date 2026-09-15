031008b88cdb002f0ad7eb8ffbea4622f0ead3f4
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2026-09-15 23:22:25 +0200

    [cubehal] Read defines and structures of CMSIS and CubeHAL headers

---
+++ src/modm_data/cubehal/header.py
@@ -0,0 +1,283 @@
+# Copyright 2025, Niklas Hauser
+# SPDX-License-Identifier: MPL-2.0
+
+import re
+import subprocess
+import logging
+import shutil
+import pickle
+
+from pathlib import Path
+from jinja2 import Environment
+import cxxheaderparser.simple
+import cxxheaderparser.types
+
+from ..utils import ext_path
+
+
+_LOGGER = logging.getLogger(__file__)
+
+_CUBE_PATH = ext_path("stmicro/cubehal")
+_HEADER_PATH = ext_path("stmicro/header")
+_CMSIS_PATH = ext_path("arm/cmsis/CMSIS/Core/Include")
+_CACHE_PATH = ext_path("cache/cmsis/stm32")
+
+_SUBSTITUTIONS = [
+    # (r"/\* +?(Legacy defines|Legacy aliases|Old .*? legacy purpose|Aliases for .*?) +?\*/.*?\n\n", "", re.S|re.M),
+    (r"/\* +?[Ll]egacy (aliases|defines|registers naming) +?\*/.*?\n\n", "", re.S | re.M),
+    (r"/\* +?Old .*? legacy purpose +?\*/.*?\n\n", "", re.S | re.M),
+    (r"/\* +?Aliases for .*? +?\*/.*?\n\n", "", re.S | re.M),
+    # (r"( 0x[0-9A-F]+)\)                 ", r"\1U", re.S|re.M),
+    (r"#define[^<]+?/\*!<.*? [Ll]egacy .*?\*/\n", "", 0),
+]
+
+_HEADER_TEMPLATE = r"""
+#include <iostream>
+#include <concepts>
+#include <cstdint>
+#include <climits>
+
+{% for name, value in defines | items | sort %}
+#define {{name}} {{value}}{% endfor %}
+
+void __dm_f(char const *symbol, std::integral auto value) {
+    std::cout << "\"" << symbol << "\": " << value << ",\n";
+}
+void __dm_f(char const *symbol, unsigned char value) {
+    std::cout << "\"" << symbol << "\": " << (uint16_t)value << ",\n";
+}
+void __dm_f(char const *symbol, const char *value) {
+    std::cout << "\"" << symbol << "\": \"" << value << "\",\n";
+}
+#define __dm(def) __dm_f(#def, (def));
+int main()
+{
+std::cout << "defines = {";{% for name, value in defines | items | sort %}
+__dm({{name}}){% endfor %}
+std::cout << "}";
+return 0;
+}
+"""
+
+
+def _get_define_for_device(did, familyDefines):
+    """
+    Returns the STM32 specific define from an identifier
+    """
+    if len(familyDefines) == 1:
+        return familyDefines[0]
+
+    devName = f"STM32{did.family.upper()}{did.name.upper()}"
+
+    # Map STM32F7x8 -> STM32F7x7
+    if did.family == "f7" and devName[8] == "8":
+        devName = devName[:8] + "7"
+
+    deviceDefines = [d for d in familyDefines if d.startswith(devName)]
+    # if there is only one define thats the one
+    if len(deviceDefines) == 1:
+        return deviceDefines[0]
+
+    # sort with respecting variants
+    minlen = min(len(d) for d in deviceDefines)
+    deviceDefines.sort(key=lambda d: (d[:minlen], d[minlen:]))
+
+    # now we match for the size-id (and variant-id if applicable).
+    devNameMatch = f"{devName}x{did.size.upper()}"
+    if did.family == "l1":
+        # Map STM32L1xxQC and STM32L1xxZC -> STM32L162QCxA variants
+        if did.pin in ["q", "z"] and did.size == "c":
+            devNameMatch += "A"
+        else:
+            devNameMatch += did.variant.upper()
+    elif did.family == "h7":
+        devNameMatch = devNameMatch[:-1] + "x"
+        if did.variant:
+            devNameMatch += did.variant.upper()
+    for define in deviceDefines:
+        if devNameMatch <= define:
+            return define
+
+    # now we match for the pin-id.
+    devNameMatch = f"{devName}{did.pin.upper()}x"
+    for define in deviceDefines:
+        if devNameMatch <= define:
+            return define
+
+    return None
+
+
+def _read_cpp_header(
+    core: str, device: str, includes: list[Path], headers: list[Path], options: str = None
+) -> dict[str, str]:
+    cmd = f"arm-none-eabi-gcc -E -mcpu=cortex-m{core} -D {device} {options or ''}"
+    for p in includes:
+        cmd += f" -I {p}"
+    for h in headers:
+        cmd += f" {h}"
+    _LOGGER.debug(cmd)
+    output = subprocess.run(cmd, shell=True, capture_output=True)
+    assert not output.returncode, output.stderr.decode("utf-8")
+    return output.stdout.decode("utf-8")
+
+
+def _read_defines(core: str, device: str, includes: list[Path], headers: list[Path]) -> dict[str, str]:
+    output = _read_cpp_header(core, device, includes, headers, "-dM")
+    _LOGGER.debug(output)
+    defines = {}
+    for line in output.splitlines():
+        name, value = line[8:].split(" ", 1)
+        defines[name] = value
+    return defines
+
+
+def _resolve_defines(device, includes, defines, recursive=20):
+    source = (_CACHE_PATH / f"{device[:7].lower()}xx" / device).with_suffix(".cpp").absolute()
+
+    if not (runner := source.with_suffix(".run")).exists():
+        _LOGGER.info(f"Generating '{source.name}'...")
+        template = Environment().from_string(_HEADER_TEMPLATE)
+        source.write_text(template.render({"defines": defines}))
+
+        _LOGGER.info(f"Compiling '{source.name}'...")
+        cmd = f"g++-13 -std=c++20 -Wno-narrowing -fms-extensions -fext-numeric-literals -D {device}"
+        for inc in includes:
+            cmd += f" -I {inc}"
+        cmd += f" -o {runner} {source}"
+        _LOGGER.debug(cmd)
+        output = subprocess.run(cmd, shell=True, capture_output=True)
+        if output.returncode:
+            assert recursive > 0, "Failed to compile"
+            output = output.stderr.decode("utf-8")
+            for missing in re.findall(r"error: ('.+?' was not declared in this scope).+?", output):
+                _LOGGER.debug(missing)
+            ignore = re.findall(r"\| +__dm\((.+?)\)", output)
+            ignore += re.findall(r"warning: \"(.+?)\" redefined", output)
+            _LOGGER.info(f"Removing {ignore}")
+            defines = {k: v for k, v in defines.items() if k not in ignore}
+            return _resolve_defines(device, includes, defines, recursive - 1)
+
+    _LOGGER.info(f"Running '{runner.name}'...")
+    output = subprocess.run([str(runner)], shell=True, capture_output=True)
+    localv = {}
+    exec(output.stdout, globals(), localv)
+    return localv["defines"]
+
+
+def _copy_headers(outdir, files):
+    for file in files:
+        if not (outfile := (outdir / file.name)).exists():
+            content = file.read_text(encoding="utf-8-sig", errors="replace")
+            for pattern, subs, flags in _SUBSTITUTIONS:
+                content = re.sub(pattern, subs, content, flags=flags)
+            outfile.write_text(content)
+
+
+def _read_header(device_define):
+    family = f"{device_define[:7].lower()}xx"
+    header_inc = _HEADER_PATH / family / "Include"
+    cube_inc = _CUBE_PATH / family / "Inc"
+
+    (header_cache := (_CACHE_PATH / family)).mkdir(parents=True, exist_ok=True)
+    includedirs = [_CMSIS_PATH, header_cache]
+
+    # Copy over all header files and clean them up
+    if not (dst := header_cache / "Legacy").exists() and (src := cube_inc / "Legacy").exists():
+        shutil.copytree(src, dst)
+        for file in dst.glob("*.h"):
+            file.write_text("")
+    _copy_headers(header_cache, header_inc.glob("*.h"))
+    _copy_headers(header_cache, cube_inc.glob("*.h"))
+
+    device_header = header_cache / f"{device_define.lower()}.h"
+    core_header, core = re.search(r'#include "(core_cm(.+?).h)"', device_header.read_text()).groups()
+    header_defines = _read_defines(core, device_define, includedirs, [device_header])
+
+    # ARM CMSIS header files
+    core_defines = _read_defines(core, device_define, [_CMSIS_PATH], [_CMSIS_PATH / core_header])
+
+    ll_headers = list(header_cache.glob("*_ll_*.h"))
+    ll_defines = _read_defines(core, device_define, includedirs, ll_headers)
+
+    cube_headers = list(header_cache.glob("*_hal_*.h"))
+    all_defines = _read_defines(core, device_define, includedirs, cube_headers)
+
+    value_defines = {
+        k: v for k, v in all_defines.items() if v and "*" not in v and "(" not in k and not k.startswith("__")
+    }
+    value_defines = _resolve_defines(device_define, includedirs, value_defines)
+
+    device_header_content = _read_cpp_header(
+        core, device_define, includedirs, [device_header], "-P -D __inline= -D __extension__= -D __restrict="
+    )
+    cxxheader = cxxheaderparser.simple.parse_string(device_header_content)
+
+    peripherals = {}
+    clanons = {
+        c.class_decl.typename.segments[0].id: c
+        for c in cxxheader.namespace.classes
+        if isinstance(c.class_decl.typename.segments[0], cxxheaderparser.types.AnonymousName)
+    }
+    for typedef in cxxheader.namespace.typedefs:
+        if typedef.name.endswith("_TypeDef"):
+            fields = []
+            tclass = clanons[typedef.type.typename.segments[0].id]
+            for field in tclass.fields:
+                if isinstance(field.type, cxxheaderparser.types.Array):
+                    fields.append(
+                        (
+                            field.name,
+                            field.type.array_of.typename.segments[0].name,
+                            int(field.type.size.tokens[0].value.format()),
+                        )
+                    )
+                else:
+                    fields.append((field.name, field.type.typename.segments[0].name, None))
+            peripherals[typedef.name] = fields
+
+    irq_vectors = [(int(i.value.format()), i.name[:-5]) for i in cxxheader.namespace.enums[0].values]
+
+    return {
+        "device_define": device_define,
+        "core": core_defines,
+        "header": header_defines,
+        "ll": ll_defines,
+        "cube": all_defines,
+        "defines": value_defines,
+        "irqs": irq_vectors,
+        "peripherals": peripherals,
+    }
+
+
+def read_header(did):
+    """
+    Finds all register and bit names in the CMSIS header file.
+
+    :returns: a RegisterMap object that allows regex-ing for register names.
+    """
+    family_folder = f"stm32{did.family}xx"
+    if did.string[5:8] in ["h7r", "h7s"]:
+        family_folder = "stm32h7rsxx"
+    elif did.string[5:8] == "wba":
+        family_folder = "stm32wbaxx"
+    family_header = f"{family_folder}.h"
+    if did.string[5:8] == "wb0":
+        family_folder = "stm32wb0xx"
+        family_header = "stm32wb0x.h"
+    elif did.string[5:8] == "wl3":
+        family_folder = "stm32wl3xx"
+        family_header = "stm32wl3x.h"
+    family_header = (_HEADER_PATH / family_folder / "Include" / family_header).read_text(
+        encoding="utf-8", errors="replace"
+    )
+    match = re.findall(r"if +defined\( *(STM32[A-Z][\w\d]+) *\)", family_header)
+    assert match, f"No CPP define match found for '{did.string}'!"
+    device_define = _get_define_for_device(did, match)
+    assert device_define, f"No device define found for '{did.string}'!"
+
+    if (pkl := _CACHE_PATH / family_folder / f"{device_define}.pkl").exists():
+        return pickle.loads(pkl.read_bytes())
+
+    values = _read_header(device_define)
+    pkl.write_bytes(pickle.dumps(values, protocol=pickle.HIGHEST_PROTOCOL))
+    return values
